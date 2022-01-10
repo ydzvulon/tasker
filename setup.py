dca099be80b36dff406e17826b6d4ee81a143de3
Yair Dar <yair.dar@outlook.com>
2022-01-10 05:40:51 +0000

    bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 # "hello",
 setup(
     name="pytasker",
-    version="0.1.5",
+    version="0.1.6",
     description="Greet the world.",
     py_modules=["pytasker"],
     entry_points={
